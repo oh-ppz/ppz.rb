c4eada9e62b25525f3b80890c56f9c14b50c1448
gy <372301467@qq.com>
2021-02-02 16:18:10 +0800

    文档

--- app/ruby/lib/object/model/special-block/container.rb
+++ app/ruby/lib/object/model/special-block/container.rb
@@ -1,10 +1,10 @@
 require_relative '../abstract/wrapper-model'
 
 class SpecialContainerModel < AbstractWrapperModel
-  REG_EXP = /^``` (.+)/
+  REG_EXP = /^```( (.*))?/
   def self.from_line line
     return nil unless match_data = REG_EXP.match(line)
-    SpecialContainerModel.new $1
+    SpecialContainerModel.new $2
   end
 
   def initialize name
