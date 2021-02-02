ce58c719027990784a2ea09ecb75d92f49e7c8ad
gy <372301467@qq.com>
2021-02-02 17:11:53 +0800

    转义字符 bug

--- app/ruby/lib/object/model/p/index.rb
+++ app/ruby/lib/object/model/p/index.rb
@@ -2,7 +2,13 @@ require_relative '../abstract/model'
 
 class PModel < AbstractModel
   def initialize text
-    @text = transform_inline_element text
+    # 转义行首的加号
+    pre = text[0..2]
+    text = text[1..-1] if (pre == '\\+ ' or pre == '\\> ')
+    # 转义 html
+    text = transform_inline_element text
+
+    @text = text
   end
 
   def to_html
