ba01706265840afab3e5069f2777c67a8c083c0a
gy <372301467@qq.com>
2021-02-04 16:32:05 +0800

    完善单文件输出

--- app/ruby/bin/common.rb
+++ app/ruby/bin/common.rb
@@ -2,34 +2,41 @@ require 'pathname'
 
 module PPZMain
   CURRENT_PATH = File.dirname __FILE__
+  WORK_DIRECTORY = Pathname Dir.pwd
   CSS_FILE_PATH = (Pathname CURRENT_PATH) + '../asset/style/ppz.css'
 
   class Util
     class << self
-      def get_file_in_and_out
-        file_in, file_out = ARGV
-        unless file_in && file_out
-          abort '参数不对啊，需要至少俩参数（输入文件路径、输出文件路径）'
+      def get_in_and_out
+        target_in, target_out = ARGV
+
+        # + 输入文件
+        abort '要编译哪那个文件？请告诉我' unless target_in # 检查参数存在
+        target_in = (WORK_DIRECTORY + target_in).to_s # 绝对路径
+        unless File.exist? target_in # 不存在的话，看看加上 .ppz 后是否存在
+          target_in += '.ppz'
+          abort target_in[0..-5] + ' 不存在' unless File.exist? target_in # 还不存在的话，就说明是写错了
         end
+        abort '尚未支持编译文件夹' if File.directory? target_in
 
-        current_path = Pathname Dir.pwd
-        file_in = (current_path + file_in).to_s
-        file_out = (current_path + file_out).to_s
-
-        unless File.exist? file_in
-          abort '没有 ' + file_in + " 这个文件\n【参数不正确：ppz 源文件不存在】"
+        # + 输出文件
+        unless target_out
+          # 从输入文件获取文件名
+          target_out = (/(.*).ppz$/.match target_in)?$1:target_in
         end
-
-        file_out_dir = File.dirname file_out
-        unless Dir.exist? file_out_dir 
-          abort '输出文件的目录不存在'
+        target_out = WORK_DIRECTORY + target_out
+        # ++ 检查上级文件夹是否存在
+        upper_dir = (target_out + '..').to_s
+        abort upper_dir + ' 目录不存在' unless Dir.exist? upper_dir
+        # ++ 检查文件夹：有则检查里面有没有文件；无则创建文件夹
+        target_out = target_out.to_s
+        if Dir.exist? target_out
+          abort target_out + ' 不是一个空文件夹' unless (Dir.children target_out).size == 0
+        else
+          Dir.mkdir target_out
         end
-
-        if File.exist? file_out
-          abort '输出文件已存在'
-        end
-
-        [file_in, file_out]
+        
+        [target_in, target_out]
       end
     end
   end
