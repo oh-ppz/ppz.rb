41a341827f482612b04e4dd20980f698af763b33
gy <372301467@qq.com>
2021-02-20 15:51:49 +0800

    update

--- app/lib/parser/folder/index.rb
+++ app/lib/parser/folder/index.rb
@@ -1,12 +1,6 @@
 # 解析一个文件夹里的 .ppz 文件
 
 module PPZ::Folder
-  class FolderParser
-    def initialize
-      @context = Context.new
-    end
-  end
-
   require_relative './model/abstract'
   require_relative './model/folder'
   require_relative './model/file/abstract'
